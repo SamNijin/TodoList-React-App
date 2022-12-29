5a256fc95813d62368b3a74447a07e0a6f90c156
SamNijin <samnijin562@gmail.com>
2022-12-29 21:02:21 +0530

    Added UI

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { v4 as uuid4 } from 'uuid'
 
 function Form({ input, setInput, todo, setTodo }) {
+
     const inputHandler = e => {
         setInput(e.target.value)
     }
@@ -11,12 +12,16 @@ function Form({ input, setInput, todo, setTodo }) {
         setInput('')
     }
     return (
-        <div>
-            <form onSubmit={submitHandler}>
-                <label htmlFor="addData">Add Items</label>
-                <input type="text" value={input} onChange={inputHandler} />
-                <button onClick={submitHandler}>Submit</button>
+        <div className='bg-purple-200 max-w-md py-10 mx-auto lg:mt-8 rounded-xl shadow-2xl mb-7 transition-all duration-200'>
+            <h2 className='text-base sm:text-xl lg:text-2xl font-medium sm:font-semibold lg:font-bold text-red-500 py-1 sm:py-3 lg:py-5'>Add New Items</h2>
+            <form onSubmit={submitHandler} className='my-2 sm:my-3 md:my-4 lg:my-5'>
+                <input type="text" value={input} placeholder='Enter Items' onChange={inputHandler} className='w-3/5 sm:w-1/2 md:w-2/5 lg:w-1/2  px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring ' required autoFocus />
             </form>
+            {
+                input === '' ?
+                    <button onClick={submitHandler} disabled className='bg-gray-500 px-3 py-1 rounded-lg text-lg font-semibold uppercase text-black teansition-all duration-1000'>Submit</button> :
+                    <button onClick={submitHandler} className='bg-green-500 px-3 py-1 rounded-lg text-lg font-semibold text-white uppercase transition-all duration-1000'>Submit</button>
+            }
         </div>
     )
 }
