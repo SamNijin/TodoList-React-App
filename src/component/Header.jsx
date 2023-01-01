3f250c22df2422d45bb0dc17d3af2b7de7d5cdce
SamNijin <samnijin562@gmail.com>
2023-01-01 13:18:01 +0530

    Cleared Add Bug

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import icon from '../image/task-list.png'
+import React from "react";
+import icon from "../image/task-list.png";
 
 function Header() {
     return (
-        <div className='bg-purple-900 h-6 sm:h-10 lg:h-16 flex justify-center gap-1 sm:gap-3 lg:gap-4 pt-1 lg:pt-3 transition-all duration-200'>
-            <img src={icon} className='h-4 sm:h-8 lg:h-10' alt="todoList" />
-            < h2 className='text-purple-200 text-xs sm:text-base lg:text-2xl sm:font-bold lg:font-extrabold sm:pt-1' > ToDo List</ h2>
-        </div >
-    )
+        <div className="bg-purple-900 h-6 sm:h-10 lg:h-16 flex justify-center gap-1 sm:gap-3 lg:gap-4 pt-1 lg:pt-3 transition-all duration-200">
+            <img src={icon} className="h-4 sm:h-8 lg:h-10" alt="todoList" />
+            <h2 className="text-purple-200 text-xs sm:text-base lg:text-2xl sm:font-bold lg:font-extrabold sm:pt-1">
+                ToDo List
+            </h2>
+        </div>
+    );
 }
 
-export default Header
+export default Header;
--- src/component/ViewData.jsx
+++ src/component/ViewData.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react';
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { initializeApp } from "firebase/app";
-import { doc, getDocs, deleteDoc } from 'firebase/firestore'
-import { firebaseConfig, db, connectionReference } from '../firebase/FirebaseConfig'
+import { doc, getDocs, deleteDoc } from "firebase/firestore";
+import {
+    firebaseConfig,
+    db,
+    connectionReference,
+} from "../firebase/FirebaseConfig";
 
 initializeApp(firebaseConfig);
 
@@ -10,45 +14,51 @@ function ViewData() {
     const [state, setstate] = useState([]);
 
     useEffect(() => {
-        FetchData()
-    }, []);
+        FetchData();
+    }, [state]);
 
     useEffect(() => {
-        console.log(state)
+        console.log(state);
     }, [state]);
 
     function FetchData() {
         getDocs(connectionReference)
             .then((snapshot) => {
-                const task = snapshot.docs.map(doc => ({
+                const task = snapshot.docs.map((doc) => ({
                     data: doc.data(),
-                    id: doc.id
-                }))
-                setstate(task)
-
+                    id: doc.id,
+                }));
+                setstate(task);
             })
-            .catch(err => console.log(err.message))
+            .catch((err) => console.log(err.message));
     }
 
     const deleteHandler = (id) => {
-        console.log('clicked delete', id)
-        const documentReference = doc(db, 'tasklist', id)
-        deleteDoc(documentReference)
-    }
+        console.log("clicked delete", id);
+        const documentReference = doc(db, "tasklist", id);
+        deleteDoc(documentReference);
+    };
     return (
-        <div >
+        <div>
             <h2>view from firebase</h2>
-            {
-                state.map((lists) => (
-                    <li key={lists.id} className='list-none lg:flex lg:justify-between lg:px-40'>{lists.data.task}
-                        <div className='mb-2'>
-                            <button className='px-5 py-2 rounded-md bg-purple-700' onClick={() => deleteHandler(lists.id)}>delete</button>
-                        </div>
-                    </li>
-                ))
-            }
-        </div >
-    )
+            {state.map((lists) => (
+                <li
+                    key={lists.id}
+                    className="list-none lg:flex lg:justify-between lg:px-40"
+                >
+                    {lists.data.task}
+                    <div className="mb-2">
+                        <button
+                            className="px-5 py-2 rounded-md bg-purple-700"
+                            onClick={() => deleteHandler(lists.id)}
+                        >
+                            delete
+                        </button>
+                    </div>
+                </li>
+            ))}
+        </div>
+    );
 }
 
-export default ViewData
+export default ViewData;
